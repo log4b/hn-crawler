8f7008708d239cf6f06ea9175589ba27f47b018e
Bruno Silva <bruno.csilva@b2wdigital.com>
2017-04-17 14:12:44 -0300

    Storing the result temporary on filesystem

--- src/main/java/com/br/log4b/crawler/HnVisaScraping.java
+++ src/main/java/com/br/log4b/crawler/HnVisaScraping.java
@@ -4,38 +4,73 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class HnVisaScraping {
 
     private final List<String> searchingKey;
 
+    private final Month month;
+
+    private final String finalPath;
+
+    private final String FIELD_SEPARATOR = "||";
+
     public HnVisaScraping(List<String> searchingKey) {
+        month = LocalDate.now().getMonth();
+
         if(null == searchingKey || searchingKey.size() == 0)    {
             throw new IllegalArgumentException("You should specify at least one word to be found");
+        } else {
+            this.searchingKey = searchingKey;
         }
-        this.searchingKey = searchingKey;
+
+        final String path = System.getenv("path");
+
+        if(null == path)    {
+            throw new IllegalArgumentException("You should set a 'path' environment variable");
+        } else {
+            finalPath = path + "/" + month + "/" + "visa.cvs";
+        }
     }
 
     public void start() {
         try {
-            //TODO Change the application to find the url itself considering the present month
-            final Document rootDocument = Jsoup.connect("https://news.ycombinator.com/item?id=13764728").get();
+            //TODO search for the correct URL based on current month/year
+            final Document rootDocument = Jsoup.connect("https://news.ycombinator.com/item?id=14023198").get();
 
-            final Elements comments = rootDocument.getElementsByClass("comment");
+            final Elements rootComments = rootDocument.getElementsByClass("athing");
 
-            //TODO Store data somewhere
-            comments.parallelStream()
-                    .filter(comment -> searchingKey
-                            .stream()
-                            .anyMatch(key -> comment.text().contains(key)))
-                    .collect(Collectors.toList()).forEach(u -> System.out.println(u));
+            StringBuilder builder = new StringBuilder();
 
+            rootComments
+                    .parallelStream()
+                    .forEach(r -> r.getElementsByClass("comment")
+                            .parallelStream()
+                            .filter(comment -> searchingKey.stream()
+                                    .anyMatch(key -> comment.text().contains(key)))
+                            .forEach(filtered -> builder.append(r.id())
+                                    .append(FIELD_SEPARATOR)
+                                    .append(filtered.text().replace("\n", ""))
+                                    .append("\n")));
+
+            storePost(builder.toString());
         } catch (IOException e) {
-            //TODO Add logback to dependecies and log exception
-            e.printStackTrace();
+            System.err.println("Cannot connect to the url");
+        }
+    }
+
+    private void storePost(String content)    {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(finalPath))) {
+            writer.write(content);
+        } catch (IOException e) {
+            System.err.println("An error has occurred. Cannot store the comments on path");
         }
     }
 
